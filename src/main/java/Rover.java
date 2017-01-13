d01650ceda238cae63b329e0887ecc31c867e33f
sitaram <sitaram@thoughtworks.com>
2017-01-13 12:12:43 +0530

    Refactered the Rover for getting the direction and now removed the Rover position Object.

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -5,12 +5,12 @@ public class Rover {
     private String currentPosition;
     private int xAxis, yAxis;
     char direction;
-    private RoverPosition roverPosition;
+//    private RoverPosition roverPosition;
 
     public Rover (String currentPosition){
-//        this.xAxis = currentPosition.charAt(0);
-//        this.yAxis = currentPosition.charAt(1);
-//        this.direction = currentPosition.charAt(2);
+        this.xAxis = currentPosition.charAt(0);
+        this.yAxis = currentPosition.charAt(2);
+        this.direction = currentPosition.charAt(4);
         this.currentPosition = currentPosition;
         setPosition(currentPosition);
     }
@@ -21,82 +21,82 @@ public class Rover {
         }
     }
 
+
+
+
     public char facingDirection() {
-        return this.currentPosition.charAt(2);
+        return this.direction;
     }
 
-    public void rotate(char r) {
 
-    }
-
-    private class RoverPosition {
-
-
-        public RoverPosition(int xAxis, int yAxis, char direction) {
-
-            rowerPosition.add(xAxis);
-            rowerPosition.add(yAxis);
-            rowerPosition.add(direction);
-        }
+//    private class RoverPosition {
+//
+//
+//        public RoverPosition(int xAxis, int yAxis, char direction) {
+//
+//            rowerPosition.add(xAxis);
+//            rowerPosition.add(yAxis);
+//            rowerPosition.add(direction);
+//        }
 
 //        public static ArrayList getPosition() {
 //            return rowerPosition;
 //        }
 
-//        public static char getDirection(char command){
-//            if( 'N' == (char) rowerPosition.get(2)) {
-//                if(command=='L') {
-//                    setDirection('E');
-//                } else
-//                if(command == 'R'){
-//                    setDirection('W');
-//                } else {
-//
-//                }
-//            } else
-//            if('S'== (char) rowerPosition.get(2) ){
-//                if(command =='R'){
-//                    setDirection('E');
-//                } else
-//                if(command=='L'){
-//                    setDirection('W');
-//                }
-//
-//            }
-//            else
-//            if('W' == (char) rowerPosition.get(2)){
-//                if(command =='L'){
-//                    setDirection('N');
-//                } else
-//                if(command =='R'){
-//                    setDirection('S');
-//                }
-//            }
-//            else
-//            if ('E' == (char) rowerPosition.get(2)) {
-//                if(command == 'L'){
-//                    setDirection('S');
-//                } else
-//                if(command == 'R')
-//                    setDirection('N');
-//            }
-//            return (char) rowerPosition.get(2);
-//        }
+        public char rotate(char command){
+            if( 'N' == this.direction) {
+                if(command=='L') {
+                    setDirection('E');
+                } else
+                if(command == 'R'){
+                    setDirection('W');
+                } else {
+
+                }
+            } else
+            if('S'== (char) this.direction){
+                if(command =='R'){
+                    setDirection('E');
+                } else
+                if(command=='L'){
+                    setDirection('W');
+                }
+
+            }
+            else
+            if('W' == (char) this.direction){
+                if(command =='L'){
+                    setDirection('N');
+                } else
+                if(command =='R'){
+                    setDirection('S');
+                }
+            }
+            else
+            if ('E' == (char) this.direction) {
+                if(command == 'L'){
+                    setDirection('S');
+                } else
+                if(command == 'R')
+                    setDirection('N');
+            }
+            return (char) this.direction;
+        }
+
+        private void setDirection(char direction) {
+            this.direction = direction;
+        }
+
 //
 //        private void setDirection(char e) {
 //            rowerPosition.remove(2);
 //            rowerPosition.add(e);
 //        }
 //
-//        @Override
-//        public String toString() {
-//            return "RoverPosition{" +
-//                    "rowerPosition=" + rowerPosition +
-//                    '}';
-//        }
+
 //
 //        public static char getDirection() {
-//            return (char) rowerPosition.get(2);
+//            return (char) this.direction;
 //        }
 //    }
 
@@ -116,5 +116,5 @@ public class Rover {
 //
 //    public String position() {
 //        return String.valueOf(RoverPosition.getPosition());
-    }
+//    }
 }
--- test/java/RoverTest.java
+++ test/java/RoverTest.java
@@ -13,85 +13,84 @@ public class RoverTest {
 
     }
 
-
-    //    @Test
-//    public void shouldReturnEastIfCurrentDirectionIsNorthAndCommandForSpinIsL() throws Exception {
-//        Rover rower = new Rover("1 3 N");
-//
-//        assertEquals('N',rower.facingDirection());
-//        rower.spin('L');
-//        assertEquals('E',rower.facingDirection());
-//
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnWestIfCurrentDirectionIsNorthAndCommandForSpinIsR() throws Exception {
-//        Rover rower = new Rover("1 3 N");
-//
-//        assertEquals('N',rower.facingDirection());
-//        rower.spin('R');
-//        assertEquals('W',rower.facingDirection());
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnEastIfCurrentDirectionIsSouthAndCommandForSpinIsL() throws Exception {
-//        Rover rower = new Rover("1 3 N");
-//
-//        assertEquals('S',rower.facingDirection());
-//        rower.spin('L');
-//        assertEquals('W',rower.facingDirection());
-//
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnWestIfCurrentDirectionIsSouthAndCommandForSpinIsR() throws Exception {
-//        Rover rower = new Rover("1 3 N");
-//
-//        assertEquals('S',rower.facingDirection());
-//        rower.spin('R');
-//        assertEquals('E',rower.facingDirection());
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnSouthIfCurrentDirectionIsWestAndCommandForSpinIsL() throws Exception {
-//        Rover rower = new Rover("1 3 N");
-//
-//        assertEquals('W',rower.facingDirection());
-//        rower.spin('L');
-//        assertEquals('N',rower.facingDirection());
-//
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnSouthIfCurrentDirectionIsWestAndCommandForSpinIsR() throws Exception {
-//        Rover rower = new Rover("1 3 N");
-//
-//        assertEquals('W',rower.facingDirection());
-//        rower.spin('R');
-//        assertEquals('S',rower.facingDirection());
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnSouthIfCurrentDirectionIsEastAndCommandForSpinIsL() throws Exception {
-//        Rover rower = new Rover("1 3 N");
-//
-//        assertEquals('E',rower.facingDirection());
-//        rower.spin('L');
-//        assertEquals('S',rower.facingDirection());
-//
-//
-//    }
-//
     @Test
-    public void shouldReturnNorthIfCurrentDirectionIsEastAndCommandForSpinIsR() throws Exception {
-
+    public void shouldReturnEastIfCurrentDirectionIsNorthAndCommandForSpinIsL() throws Exception {
         Rover rower = new Rover("1 3 N");
+
+        assertEquals('N',rower.facingDirection());
+        rower.rotate('L');
+        assertEquals('E',rower.facingDirection());
+
+
+    }
+
+    @Test
+    public void shouldReturnWestIfCurrentDirectionIsNorthAndCommandForSpinIsR() throws Exception {
+        Rover rower = new Rover("1 3 N");
+
+        assertEquals('N',rower.facingDirection());
+        rower.rotate('R');
+        assertEquals('W',rower.facingDirection());
+
+    }
+
+    @Test
+    public void shouldReturnWestIfCurrentDirectionIsSouthAndCommandForRotateIsL() throws Exception {
+        Rover rower = new Rover("1 3 S");
+
+        assertEquals('S',rower.facingDirection());
+        rower.rotate('L');
+        assertEquals('W',rower.facingDirection());
+
+
+    }
+
+    @Test
+    public void shouldReturnEastIfCurrentDirectionIsSouthAndCommandForRotateIsR() throws Exception {
+        Rover rower = new Rover("1 3 S");
+
+        assertEquals('S',rower.facingDirection());
+        rower.rotate('R');
+        assertEquals('E',rower.facingDirection());
+
+    }
+
+    @Test
+    public void shouldReturnNorthIfCurrentDirectionIsWestAndCommandForRotateIsL() throws Exception {
+        Rover rower = new Rover("1 3 W");
+
+        assertEquals('W',rower.facingDirection());
+        rower.rotate('L');
+        assertEquals('N',rower.facingDirection());
+
+
+    }
+
+    @Test
+    public void shouldReturnSouthIfCurrentDirectionIsWestAndCommandForRotateIsR() throws Exception {
+        Rover rower = new Rover("1 3 W");
+
+        assertEquals('W',rower.facingDirection());
+        rower.rotate('R');
+        assertEquals('S',rower.facingDirection());
+
+    }
+
+    @Test
+    public void shouldReturnSouthIfCurrentDirectionIsEastAndCommandForRotateIsL() throws Exception {
+        Rover rower = new Rover("1 3 E");
+
+        assertEquals('E',rower.facingDirection());
+        rower.rotate('L');
+        assertEquals('S',rower.facingDirection());
+
+
+    }
+
+    @Test
+    public void shouldReturnNorthIfCurrentDirectionIsEastAndCommandForRotateIsR() throws Exception {
+
+        Rover rower = new Rover("1 3 E");
 
         assertEquals('E',rower.facingDirection());
         rower.rotate('R');
